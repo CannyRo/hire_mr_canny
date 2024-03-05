96bc767a85e868e88c0d3e52fabbe2d5ec77098b
CannyRo <whatcannydev@gmail.com>
2024-03-06 00:10:18 +0100

    add and initialization of NextUI a UI library

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import {Providers} from "../providers";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -10,8 +11,12 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className='dark'>
+      <body className={inter.className}>
+      <Providers>
+        {children}
+      </Providers>
+      </body>
     </html>
   );
 }
