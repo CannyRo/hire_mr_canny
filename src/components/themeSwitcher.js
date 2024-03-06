65dab9243e1cd2e0ed9724c2dede35405a0e3dc3
CannyRo <whatcannydev@gmail.com>
2024-03-06 02:16:56 +0100

    add a themeSwitcher component

---
+++ src/components/themeSwitcher.js
@@ -0,0 +1,43 @@
+"use client";
+import React from "react";
+import {Switch} from "@nextui-org/react";
+import {MoonIcon} from "./_moonIcon";
+import {SunIcon} from "./_sunIcon";
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+
+export function ThemeSwitcher() {
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  const handleTheme = event => {
+    event.target.checked ? setTheme("dark") : setTheme("light");
+  }
+
+  if(!mounted)return null;
+
+  return (
+    <div>
+      The current theme is: {theme} <br/>
+      <Switch
+        defaultSelected
+        size="lg"
+        color="secondary"
+        thumbIcon={({ isSelected, className }) =>
+            isSelected ? (
+            <SunIcon className={className} />
+            ) : (
+            <MoonIcon className={className} />
+            )
+        }
+        onChange={handleTheme}
+        >
+        Dark mode
+        </Switch>
+    </div>
+  );
+}
