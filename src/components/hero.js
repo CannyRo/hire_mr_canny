341085633ee179b68b6da2f51f1e6a2db7242a2a
CannyRo <whatcannydev@gmail.com>
2024-03-07 12:14:01 +0100

    hero section

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,16 +1,13 @@
 import React from "react";
 import {useTranslations} from 'next-intl';
-import { Unica_One } from "next/font/google";
-
-const unica = Unica_One({weight: "400", subsets: ["latin"] });
 
 export function Hero(){
     const t = useTranslations('home');
 
     return(
-        <div className="container mx-auto bg-orange-200">
-            {/* <h1 className={`${unica.className} text-3xl`}>{t('hero.tagline01')}</h1> */}
-            <h1 className="text-2xl font-bold px-4">{t('hero.tagline01')} {t('hero.tagline02')} <span>{t('hero.keyword01')}</span></h1>
+        <div className="container py-24 px-3">
+            <h1 className="mb-4 text-3xl leading-10 font-black">{t('hero.tagline01')} {t('hero.tagline02')} <span>{t('hero.keyword01')}</span></h1>
+            <h2>Parce ce que l&apos;alternance est un choix gagnant-gagnant, n&apos;hésitez plus à économiser votre argent, votre temps, votre énergie.</h2>
         </div>
     )
 }
