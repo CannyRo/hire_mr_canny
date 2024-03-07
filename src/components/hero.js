d250dea9e41effb1f93b0c099b6fa3a25a58c3ea
CannyRo <whatcannydev@gmail.com>
2024-03-07 13:21:52 +0100

    raw french version of argument section

--- src/components/hero.js
+++ src/components/hero.js
@@ -5,9 +5,9 @@ export function Hero(){
     const t = useTranslations('home');
 
     return(
-        <div className="container py-24 px-3">
-            <h1 className="mb-4 text-3xl leading-10 font-black">{t('hero.tagline01')} {t('hero.tagline02')} <span>{t('hero.keyword01')}</span></h1>
-            <h2>Parce ce que l&apos;alternance est un choix gagnant-gagnant, n&apos;hésitez plus à économiser votre argent, votre temps, votre énergie.</h2>
+        <div className="container py-24 px-10">
+            <h1 className="mb-4 text-3xl leading-10 tracking-wide font-black">{t('hero.tagline01')}&nbsp;{t('hero.tagline02')}</h1>
+            <h2 className="text-xl">Parce que l&apos;alternance est un choix gagnant-gagnant, n&apos;hésitez plus à économiser votre argent, votre temps et votre énergie.</h2>
         </div>
     )
 }
