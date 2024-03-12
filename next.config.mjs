ff0031f6ccd877dd618abf5cf518840971f2f892
CannyRo <whatcannydev@gmail.com>
2024-03-12 22:58:34 +0100

    try to fix next config to build app

--- next.config.mjs
+++ next.config.mjs
@@ -2,10 +2,5 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-        unoptimized: true,
-    }
-};
-
+const nextConfig = {};
 export default withNextIntl(nextConfig);
