be8b775c0cf645d339c318b784e396fa41c80570
CannyRo <whatcannydev@gmail.com>
2024-03-13 00:45:45 +0100

    fix next config for building app with github page

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,8 @@ const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    basePath: "/hire_mr_canny",
     output: 'export',
+    reactStrictMode: true,
 };
 export default withNextIntl(nextConfig);
