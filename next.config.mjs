465db95a4334a5dde7d3926d10b5d2f88de445c2
CannyRo <whatcannydev@gmail.com>
2024-03-13 14:38:48 +0100

    button linked to form

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,8 @@ const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: "/hire_mr_canny",
-    output: 'export',
-    reactStrictMode: true,
+    // basePath: "/hire_mr_canny",
+    // output: 'export',
+    // reactStrictMode: true,
 };
 export default withNextIntl(nextConfig);
