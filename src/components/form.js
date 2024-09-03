887d3bcc9c2eaf1ceaf7fed0d41c3b1c7d86808f
CannyRo <69788762+CannyRo@users.noreply.github.com>
2024-09-03 08:50:46 +0200

    Update form.js
    Until updated with a captcha cause spam

--- src/components/form.js
+++ src/components/form.js
@@ -191,7 +191,7 @@ export function Form(props) {
                     onBlur={isValid}
                     isInvalid={isInvalid.message}
                 />
-                <Button variant="shadow" size="lg" className="w-full text-md bg-sky-400" onClick={sendMessage}>
+                <Button variant="shadow" size="lg" className="w-full text-md bg-sky-400" onClick={sendMessage} disabled>
                     {props.placeholderCta}
                 </Button>
             </div>
